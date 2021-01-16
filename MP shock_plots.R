a2b83d318fdf84ea735a08d8fb8b3b04ce6dde4a
Ajda Kovač <66962694+AjdaKovac@users.noreply.github.com>
2021-01-16 20:04:35 +0100

    Added MP shock plots (identical to reference article)

---
+++ MP shock_plots.R
@@ -0,0 +1,27 @@
+library(readxl)
+library(tseries)
+
+setwd("~/Documents/Github/rp-ie/data")
+
+term_structure_data <- read_excel("term_structure.xlsx")
+View(term_structure_data)
+
+### US MP shock from term yield
+
+# nov_bef = 24/11/2008, nov_aft = 25/11/2008
+
+nov_bef <- ts(term_structure_data$nov_bef, start = c(1))
+nov_aft <- ts(term_structure_data$nov_aft, start = c(1))
+ts.plot(nov_bef, nov_aft, gpars = list(col = c("black", "red")))
+
+diff_nov <- ts(term_structure_data$diff_nov, start = c(1))
+plot.ts(diff_nov, col="red")
+
+# dec_bef = 15/12/2008, dec_aft = 16/12/2008
+
+dec_bef <- ts(term_structure_data$nov_bef, start = c(1))
+dec_aft <- ts(term_structure_data$nov_aft, start = c(1))
+ts.plot(dec_bef, dec_aft, gpars = list(col = c("black", "red")))
+
+diff_dec <- ts(term_structure_data$diff_nov, start = c(1))
+plot.ts(diff_dec, col="red")
