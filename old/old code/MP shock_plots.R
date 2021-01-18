4319265ef359ef749886448e6141f93e33b52508
Ajda Kovač <66962694+AjdaKovac@users.noreply.github.com>
2021-01-18 11:42:16 +0100

    Organised files

--- old/old code/MP shock_plots.R
+++ old/old code/MP shock_plots.R
@@ -15,7 +15,7 @@ nov_aft <- ts(term_structure_data$nov_aft, start = c(1))
 ts.plot(nov_bef, nov_aft, main = "", xlab = "Maturity", ylab = "Percent", gpars = list(col = c("black", "red")))
 
 diff_nov <- ts(term_structure_data$diff_nov, start = c(1))
-plot.ts(diff_nov, col="red", main = "", xlab = "Maturity", ylab = "Percent")
+plot.ts(diff_nov, col="red", main = "LSAP-1, Nov 25, 2008", xlab = "Maturity", ylab = "Percent")
 
 # dec_bef = 15/12/2008, dec_aft = 16/12/2008
 
@@ -24,4 +24,4 @@ dec_aft <- ts(term_structure_data$nov_aft, start = c(1))
 ts.plot(dec_bef, dec_aft, main = "", xlab = "Maturity", ylab = "Percent", gpars = list(col = c("black", "red")))
 
 diff_dec <- ts(term_structure_data$diff_nov, start = c(1))
-plot.ts(diff_dec, col="red", main = "", xlab = "Maturity", ylab = "Percent")
+plot.ts(diff_dec, col="red", main = "Dec 16, 2008", xlab = "Maturity", ylab = "Percent")
